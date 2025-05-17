3260106da48f9ed87f0ad18d02d6f7b9aae42091
ithyb <90376976+ithybbr@users.noreply.github.com>
2025-05-17 13:38:33 +0500

    Delete src directory

--- src/main/java/Java/webapp/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package Java.webapp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User getByUsername(String username);
-}
